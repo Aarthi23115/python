5ebeb915d1027ac175803b5718b89a60c42254f5
E.Aarthi <aathierra37@gmail.com>
2025-10-31 14:25:16 +0530

    added repo

---
+++ app.py
@@ -0,0 +1,23 @@
+def app():
+    a = 15
+    b = 25
+    return print(a + b)
+app()
+def app():
+    a = 10
+    b = 20
+    return print(a - b)
+app()
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+temperature = int(input("Enter the temperature in Celsius: "))
+if temperature > 32:
+    print("It's a hot day.")
+elif temperature < 32:
+    print("It's a cold day.")
+else:
+    print("It's a moderate day.")
